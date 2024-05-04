9cc90ec69ae48f186306d057f44fa8b09d2165f6
Amalie <iagresho@hiof.no>
2024-05-04 23:13:59 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ export default function Home(){
         <>
         <h2>Hei, bruker</h2>
         <h3>Filmer jeg skal se!</h3>
-        <section>
+        <article>
             <img src="https://m.media-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_.jpg"></img>
             <p>Blade Runner 2049 (2017)</p>
-        </section>
+        </article>
         <section>
             <h3>Jeg skal se sammen med...</h3>
             <ul>
