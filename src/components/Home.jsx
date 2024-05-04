aabe0aae50f04d12ad77fbb306cec95e9688f83b
anineck <111729482+anineck@users.noreply.github.com>
2024-05-04 23:17:51 +0200

    endringer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,7 @@ export default function Home(){
         <>
         <h2>Hei, bruker</h2>
         <h3>Filmer jeg skal se!</h3>
-        <article>
-            <img src="https://m.media-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_.jpg"></img>
-            <p>Blade Runner 2049 (2017)</p>
-        </article>
+       
         <section>
             <h3>Jeg skal se sammen med...</h3>
             <ul>
