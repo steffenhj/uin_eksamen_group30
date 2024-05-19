1f3261accfd6a8bb0c40eab044b8b02bd23c0f35
steffenhj <xrevdoe@hotmail.com>
2024-05-19 13:44:44 +0200

    New sanity

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-    projectId: "eovw4zjg",
+    projectId: "xuamulif",
     dataset: "production",
     apiVersion: "2022-03-07",
     useCdn: true
@@ -10,9 +10,9 @@ export const client = createClient({
 //skn6jOigPlcI0NIOnggVFNFELZRH1vJq1Chfc63K35A8k4WkatNj1dYWIch1VXRjQ8OyMsBUEuLmq1rqIfAARMbTyGcLHDm47BiR1OlAEjjDa31qVmAPmD33h5OQeIJshl6ah4RTFxZ6XSwgUVKeOPpPDKlGwl3vvXNQPmXsxvUEsvjZ5VRe
 
 export const writeClient = createClient({
-    projectId: "eovw4zjg",
+    projectId: "xuamulif",
     dataset: "production",
     useCdn: false,
     apiVersion: "2022-03-07",
-    token: "skDN5Ea2vARlTTxJRIA0FtX7nvAp05N46JppsEIOIym50PLVIclUjLcwYVDfnDu7iyJvWMixkvZdSIWdBEulOtcgPCKOuID904flYPucZvkKeaFEpdvVbL2wf3bohUql9L9Pu4MmW6rLD5V43S1GGr2b02AOYlnwU54Igg5rbLnO9XyUwLGV"
+    token: "skfC3GrTcSG3Jz1tSED8tL1hy7PvoMllGZ7TJUEBRsMoGWaXUSaCGaixS5PFdQsrLNMj8TUR6fMHNlgli6UwmYmDAe1ujfTXS8kCiZBD6A81EGdwTYnK6CI0nngJ0t263IFbFAHC8kfkjxO1KS2ga2nVqchkKfE7BuGVGgOWbmJhjXMqkLS5"
   })
